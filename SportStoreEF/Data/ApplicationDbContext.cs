dafc9b76cbb2d60f5312d5c7ccabae4f42ddb397
Michiel Schoofs <michiel.schoofs@student.hogent.be>
2019-01-07 18:11:57 +0100

    “Add class City and mapping”

--- SportStoreEF/Data/ApplicationDbContext.cs
+++ SportStoreEF/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ using SportStoreEF.Data.Mappers;
 namespace SportStoreEF {
     class ApplicationDbContext :DbContext{
         DbSet<Product> products { get; set; }
+        DbSet<City> City { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             var connectionString = @"Server=.\SQLEXPRESS;Database=SportStoreex7;Integrated Security=True";
             optionsBuilder.UseSqlServer(connectionString);
@@ -12,7 +14,8 @@ namespace SportStoreEF {
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration<Product>(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration<Product>(new ProductConfiguration())
+                .ApplyConfiguration<City>(new CityConfiguration());
         }
     }
 }
---
+++ SportStoreEF/Data/Mappers/CityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsStore.Models;
+
+namespace SportStoreEF.Data.Mappers {
+    class CityConfiguration : IEntityTypeConfiguration<City> {
+        public void Configure(EntityTypeBuilder<City> builder) {
+            builder.ToTable("City");
+
+            builder.Property(c => c.Postalcode).HasMaxLength(5);
+            builder.HasKey(c => c.Postalcode);
+
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
